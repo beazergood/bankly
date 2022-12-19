62aa85a444e21c831eb4dc40e250dad3a1008e34
Dave Beazer <hi@davebeazer.dev>
2022-12-19 16:33:45 +0100

    chore: add tests for loading & error states

--- src/components/transactions/index.test.tsx
+++ src/components/transactions/index.test.tsx
@@ -42,10 +42,56 @@ describe("transaction history", () => {
 
     expect(screen.getByText("-20.25")).toBeInTheDocument();
 
-    incomeTabTrigger.click();
-
-    expect(incomeTabTrigger).toHaveAttribute("data-state", "active");
-    expect(expensesTabTrigger).toHaveAttribute("data-state", "inactive");
+      incomeTabTrigger.click();
+  
+      expect(incomeTabTrigger).toHaveAttribute("data-state", "active");
+      expect(expensesTabTrigger).toHaveAttribute("data-state", "inactive");
     expect(screen.queryByText("-20.25")).not.toBeInTheDocument();
+describe("loading & error states", () => {
+  test("should show a loading indicator when transactions are loading...", () => {
+    render(<TransactionHistory />);
+
+    expect(screen.getByText("Transaction History")).toBeInTheDocument();
+
+    const expensesTabTrigger = screen.getByRole("tab", {
+      name: "Expenses",
+    });
+
+    expect(expensesTabTrigger).toHaveAttribute("data-state", "active");
+
+    const expensesTable = screen.getByRole("table", {
+      name: "Expenses",
+    });
+
+    expect(expensesTable).toBeInTheDocument();
+    expect(screen.getByTestId("loading-element")).toBeInTheDocument();
+  });
+
+  test("should show an error state when the server returns an error 💥", async () => {
+
+    server.use(
+      rest.get("/api/transactions", (req, res, ctx) => {
+        return res(ctx.delay(), ctx.status(500), ctx.json([]))
+      })
+    );
+
+    render(<TransactionHistory />);
+
+    expect(screen.getByText("Transaction History")).toBeInTheDocument();
+
+    const expensesTabTrigger = screen.getByRole("tab", {
+      name: "Expenses",
+    });
+
+    expect(expensesTabTrigger).toHaveAttribute("data-state", "active");
+
+    const expensesTable = screen.getByRole("table", {
+      name: "Expenses",
+    });
+
+    expect(expensesTable).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByTestId("error-message")).toBeInTheDocument();
+    });
   });
 });
