d113979499926b55052568df50ba8926d68f271d
Dave Beazer <hi@davebeazer.dev>
2022-12-19 21:55:01 +0100

    refactor: break out widths into container class, tweak header margin, add dark color

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,8 +2,8 @@ import { AccountsContainer } from "../../components/accounts/accounts.viewModel"
 import { TransactionsContainer } from "../../components/transactions/transactions.viewModel";
 
 export const Home = () => (
-  <>
+  <div className="container">
     <AccountsContainer />
     <TransactionsContainer />
-  </>
+  </div>
 );
